8e1fedcc785a0a7a857ae3d70f37e03ae7339c07
Azu Emeka <azuchukwuemeka9@gmail.com>
2023-03-23 15:00:36 -0700

    tasks

---
+++ 0x04-more_functions_nested_loop/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Gets product of two numbers
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: Product of a and b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
