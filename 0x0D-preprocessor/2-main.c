074204f70f8979a498dd17c9f9685a3d29ac66a5
Azu Emeka <azuchukwuemeka9@gmail.com>
2023-04-17 14:23:16 -0700

    Task 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This prints the name of the file the program was compiled from
+ * Return: This Always returns 0
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
